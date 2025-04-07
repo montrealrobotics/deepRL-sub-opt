f85be0306d1aea357af8d0a910eb48e116d50ab0
Neo-X <glen.berseth@mila.quebec>
2025-04-07 14:30:20 -0400

    Editing launch file

--- launch.sh
+++ launch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+## Run with sbatch --array=1-2 launch.sh
 
 #SBATCH --partition=long-cpu                             # Ask for unkillable job
-#SBATCH --cpus-per-task=16                               # Ask for 2 CPUs
-#SBATCH --mem=64G                                        # Ask for 10 GB of RAM
+#SBATCH --cpus-per-task=4                               # Ask for 2 CPUs
+#SBATCH --mem=32G                                        # Ask for 10 GB of RAM
 #SBATCH --time=2:55:00                                   # The job will run for 3 hours
 #SBATCH -o /network/scratch/g/glen.berseth/slurm-%j.out  # Write the log on scratch
 
@@ -10,8 +11,8 @@ module load cudatoolkit/12.1 miniconda/3
 conda activate cleanrl
 
 python cleanrl/dqn.py --seed $SLURM_ARRAY_TASK_ID --env-id MinAtar/SpaceInvaders-v0 --track --wandb-project-name sub-optimality
-python cleanrl/dqn.py --seed $SLURM_ARRAY_TASK_ID --env-id MinAtar/Asterix-v0 --track --wandb-project-name sub-optimality
-python cleanrl/dqn.py --seed $SLURM_ARRAY_TASK_ID --env-id LunarLander-v2 --track --wandb-project-name sub-optimality
+# python cleanrl/dqn.py --seed $SLURM_ARRAY_TASK_ID --env-id MinAtar/Asterix-v0 --track --wandb-project-name sub-optimality
+# python cleanrl/dqn.py --seed $SLURM_ARRAY_TASK_ID --env-id LunarLander-v2 --track --wandb-project-name sub-optimality
 
 # python cleanrl/ppo.py --seed $SLURM_ARRAY_TASK_ID --env-id MinAtar/SpaceInvaders-v0 --track --wandb-project-name sub-optimality
 # python cleanrl/ppo.py --seed $SLURM_ARRAY_TASK_ID --env-id MinAtar/Asterix-v0 --track --wandb-project-name sub-optimality
