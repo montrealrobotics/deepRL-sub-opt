b86e73e204eb1c761853436bc42c369bc0950c54
Neo-X <glen.berseth@mila.quebec>
2025-04-18 09:56:23 -0400

    Updating ppo

--- cleanrl/ppo_continuous_action.py
+++ cleanrl/ppo_continuous_action.py
@@ -43,7 +43,7 @@ class Args:
     # Algorithm specific arguments
     env_id: str = "HalfCheetah-v4"
     """the id of the environment"""
-    total_timesteps: int = 1000000
+    total_timesteps: int = 10000000
     """total timesteps of the experiments"""
     learning_rate: float = 3e-4
     """the learning rate of the optimizer"""
