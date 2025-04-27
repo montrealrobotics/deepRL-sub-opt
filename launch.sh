16b595b62843dea3202e2c9b83966392975cafb5
Neo-X <glen.berseth@mila.quebec>
2025-04-27 17:42:39 -0400

    Working on job launching scripts

--- launch.sh
+++ launch.sh
@@ -9,7 +9,6 @@
 
 module load cudatoolkit/12.1 miniconda/3
 conda activate cleanrl
-
 echo $ALG
 echo $ENV_ID
 python $ALG --seed $SLURM_ARRAY_TASK_ID --env-id $ENV_ID --track --wandb-project-name sub-optimality
