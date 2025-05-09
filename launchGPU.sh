9273716b5356077fe061a06643ea94f85ef43f62
Neo-X <glen.berseth@mila.quebec>
2025-05-08 23:01:36 -0400

    Working on job launching

--- launchGPU.sh
+++ launchGPU.sh
@@ -3,11 +3,11 @@
 
 #SBATCH --partition=long                                # Ask for unkillable job
 #SBATCH --cpus-per-task=4                               # Ask for 2 CPUs
-#SBATCH --ntasks=3                                      # Ask for 2 tasks per GPU 
+#SBATCH --ntasks=2                                      # Ask for 2 tasks per GPU 
 #SBATCH --gres=gpu:1                                    # Ask for GPUs
 #SBATCH --mem=96G                                       # Ask for 10 GB of RAM
 #SBATCH --time=5:55:00                                  # The job will run for 3 hours
-#SBATCH --tres-per-task=gpu:1                            # The job name
+
 #SBATCH -o /network/scratch/g/glen.berseth/slurm-%j.out  # Write the log on scratch
 
 module load cudatoolkit/12.1 miniconda/3
