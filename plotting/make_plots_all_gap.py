083b5ec8f02d4e68d521ee7fef1322c797863875
Neo-X <glen.berseth@mila.quebec>
2025-05-17 16:56:09 -0400

    Adding plots

---
+++ plotting/make_plots_all_gap.py
@@ -0,0 +1,149 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns; sns.set(font_scale=1.2)
+
+from make_plots import *
+
+if __name__ == '__main__':
+
+    res = 20
+    lw_ = 3
+    fig, (ax3) = plt.subplots(1, 1, figsize=(8,5))
+    #*******************************************************************************
+    #####################
+    ##### Global Optimality #####
+    #####################
+    #*******************************************************************************
+    
+    datadir = './data/PPO_MR-all.csv'
+    df = pd.read_csv(datadir)
+    title = 'Global Optimality Gap for Montezumas Revenge'
+    ax3.set_title(title)
+
+    jobs = [
+        "MontezumaRevengeNoFrameskip-v4__ppo_atari__0__0__1747414034",
+        "MontezumaRevengeNoFrameskip-v4__ppo_atari__0__6__1747414032",
+        "MontezumaRevengeNoFrameskip-v4__ppo_atari__0__5__1747414032",
+        "MontezumaRevengeNoFrameskip-v4__ppo_atari__0__4__1747414032",
+        "MontezumaRevengeNoFrameskip-v4__ppo_atari__0__3__1747414032",
+        "MontezumaRevengeNoFrameskip-v4__ppo_atari__0__1__1747414032",
+        "MontezumaRevengeNoFrameskip-v4__ppo_atari__0__0__1747414032"
+    ]
+
+    #####################
+    ##### w/ Optimal ######
+
+    # steps_ = deNan(df["MontezumaRevengeNoFrameskip-v4__ppo_atari__0__1__1747414032 - global_step"].to_numpy())
+
+    # steps_ = np.array([np.mean(steps_[i:i+res]) for i in range(0, len(steps_)-res+1, res)])
+    # plot_data = pd.DataFrame([(step_, 50) for step_ in steps_])
+
+    # label='Expert'
+    # plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
+    # sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label])
+    # ax3.lines[-1].set_linestyle(linestyle[label])
+
+    
+    #####################
+    ##### w/ \pi ######
+    #####################
+    
+    plot_data = get_data_frame(df, key=" - charts/global_optimality_gap", res=res, jobs=jobs)
+
+    label='Global Gap'
+    plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
+    sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
+    ax3.lines[-1].set_linestyle(linestyle[label])
+
+
+    #####################
+    ##### w/ \pi deterministic ######
+    #####################
+    
+    plot_data = get_data_frame(df, key=" - charts/local_optimality_gap", res=res, jobs=jobs)
+
+    label='Local Gap'
+    plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
+    sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
+    ax3.lines[-1].set_linestyle(linestyle[label])
+    
+    ax3.ticklabel_format(axis= 'x', style='sci', scilimits=(0,3))
+    ax3.set(ylabel='Return')
+    ax3.set(xlabel='Steps x $10^4$')
+    ax3.legend()
+    #plt.subplots_adjust(bottom=.25, wspace=.25)
+    plt.show()
+    fig.savefig(title+".svg")
+    fig.savefig(title+".png")
+    fig.savefig(title+".pdf")
+
+
+    fig, (ax3) = plt.subplots(1, 1, figsize=(8,5))
+    #*******************************************************************************
+    #####################
+    ##### Global Optimality #####
+    #####################
+    #*******************************************************************************
+    
+    datadir = './data/PPO-halfCheetah.csv'
+    df = pd.read_csv(datadir)
+    title = 'Global Optimality Gap for HalfCheetah'
+    ax3.set_title(title)
+
+    jobs = [
+        "HalfCheetah-v4__ppo_continuous_action__1__1747414032",
+        "HalfCheetah-v4__ppo_continuous_action__2__1747414032",
+        "HalfCheetah-v4__ppo_continuous_action__3__1747414032",
+        "HalfCheetah-v4__ppo_continuous_action__4__1747414032",
+    ]
+
+    #####################
+    ##### w/ Optimal ######
+
+    # steps_ = deNan(df["MontezumaRevengeNoFrameskip-v4__ppo_atari__0__1__1747414032 - global_step"].to_numpy())
+
+    # steps_ = np.array([np.mean(steps_[i:i+res]) for i in range(0, len(steps_)-res+1, res)])
+    # plot_data = pd.DataFrame([(step_, 50) for step_ in steps_])
+
+    # label='Expert'
+    # plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
+    # sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label])
+    # ax3.lines[-1].set_linestyle(linestyle[label])
+
+    
+        #####################
+    ##### w/ \pi ######
+    #####################
+    
+    plot_data = get_data_frame(df, key=" - charts/global_optimality_gap", res=res, jobs=jobs)
+
+    label='Global Gap'
+    plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
+    sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
+    ax3.lines[-1].set_linestyle(linestyle[label])
+
+
+    #####################
+    ##### w/ \pi deterministic ######
+    #####################
+    
+    plot_data = get_data_frame(df, key=" - charts/local_optimality_gap", res=res, jobs=jobs)
+
+    label='Local Gap'
+    plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
+    sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
+    ax3.lines[-1].set_linestyle(linestyle[label])
+    
+    
+    ax3.ticklabel_format(axis= 'x', style='sci', scilimits=(0,3))
+    ax3.set(ylabel='Return')
+    ax3.set(xlabel='Steps x $10^4$')
+    ax3.legend()
+    #plt.subplots_adjust(bottom=.25, wspace=.25)
+    plt.show()
+    fig.savefig(title+".svg")
+    fig.savefig(title+".png")
+    fig.savefig(title+".pdf")
--- run_jobs.sh
+++ run_jobs.sh
@@ -24,7 +24,7 @@
 # sbatch --array=1-4 --export=ALL,ALG='cleanrl/dqn_atari.py',ENV_ID='SpaceInvadersNoFrameskip-v4',INTRINSIC_REWARDS='--intrinsic_rewards RND',ARGSS='--total_timesteps 50000000' launchGPU.sh
 # sbatch --array=1-4 --export=ALL,ALG='cleanrl/dqn_atari.py',ENV_ID='AsterixNoFrameskip-v4',INTRINSIC_REWARDS='--intrinsic_rewards RND',ARGSS='--total_timesteps 50000000' launchGPU.sh
 # sbatch --array=1-4 --export=ALL,ALG='cleanrl/dqn_atari.py',ENV_ID='MontezumaRevengeNoFrameskip-v4',INTRINSIC_REWARDS='--intrinsic_rewards RND',ARGSS='--start_e=0.5' launchGPU.sh
-# sbatch --array=1-4 --export=ALL,ALG='cleanrl/dqn_atari.py',ENV_ID='MontezumaRevengeNoFrameskip-v4',ARGSS='--start_e=0.5 --network_type ResNet --total_timesteps 10000000' launchGPU.sh
+sbatch --array=1-4 --export=ALL,ALG='cleanrl/dqn_atari.py',ENV_ID='MontezumaRevengeNoFrameskip-v4',ARGSS='--start_e=0.5 --network_type ResNet --total_timesteps 50000000' launchGPU.sh
 
 # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID='SpaceInvadersNoFrameskip-v4',ARGSS='--total_timesteps 50000000' launchGPU.sh
 # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID='AsterixNoFrameskip-v4',ARGSS='--total_timesteps 50000000' launchGPU.sh
@@ -32,6 +32,6 @@
 # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID='SpaceInvadersNoFrameskip-v4',INTRINSIC_REWARDS='--intrinsic_rewards E3B --total_timesteps 50000000' launchGPU.sh
 # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID='AsterixNoFrameskip-v4',INTRINSIC_REWARDS='--intrinsic_rewards E3B --total_timesteps 50000000' launchGPU.sh
 # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID='MontezumaRevengeNoFrameskip-v4',INTRINSIC_REWARDS='--intrinsic_rewards E3B --total_timesteps 50000000' launchGPU.sh
-sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID='MontezumaRevengeNoFrameskip-v4',INTRINSIC_REWARDS='--intrinsic_rewards RND --total_timesteps 50000000' launchGPU.sh
-# sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID='MontezumaRevengeNoFrameskip-v4',ARGSS='--network_type ResNet --total_timesteps 50000000' launchGPU.sh
+# sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID='MontezumaRevengeNoFrameskip-v4',INTRINSIC_REWARDS='--intrinsic_rewards RND --total_timesteps 50000000' launchGPU.sh
+sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID='MontezumaRevengeNoFrameskip-v4',ARGSS='--network_type ResNet --total_timesteps 50000000' launchGPU.sh
 
