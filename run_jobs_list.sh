383b8dd881ab2385e7b1bf027a86912657c5f9ea
Neo-X <glen.berseth@mila.quebec>
2025-05-21 22:07:31 -0400

    working on scaling

--- run_jobs_list.sh
+++ run_jobs_list.sh
@@ -26,39 +26,39 @@
 # done
 
 ##Continuous RL envs
-strings=(
-    # "Walker2d-v4"
-    # "HalfCheetah-v4"
-    "Humanoid-v4"
-    # "BipedalWalker-v3"
-)
-for env in "${strings[@]}"; do
-    # echo "$env"
-    # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--total_timesteps 10000000' launch.sh
-    sbatch --array=1-5 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=16 --num_envs 4 --total_timesteps 5000000' launch.sh
-    sbatch --array=1-5 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=32 --num_envs 4 --total_timesteps 5000000' launch.sh
-    sbatch --array=1-5 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=64 --num_envs 4 --total_timesteps 5000000' launch.sh
-    sbatch --array=1-5 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=128 --num_envs 4 --total_timesteps 5000000' launch.sh
-    sbatch --array=1-5 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=256 --num_envs 4 --total_timesteps 5000000' launch.sh
-    # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=16 --use_layer_norm --num_envs 4 --total_timesteps 5000000' launch.sh
-done
-
-## Atari RL envs
 # strings=(
-#     # "MontezumaRevengeNoFrameskip-v4"
-#     # "AsterixNoFrameskip-v4"
-#     # "SpaceInvadersNoFrameskip-v4"
-#     # "PitfallNoFrameskip-v4"
-#     "BattleZoneNoFrameskip-v4"
-#     "NameThisGameNoFrameskip-v4"
-#     "PhoenixNoFrameskip-v4"
+#     # "Walker2d-v4"
+#     # "HalfCheetah-v4"
+#     "Humanoid-v4"
+#     # "BipedalWalker-v3"
 # )
 # for env in "${strings[@]}"; do
 #     # echo "$env"
-#     # sbatch --array=1-4 --export=ALL,ALG='cleanrl/dqn_atari.py',ENV_ID=$env,ARGSS='--total_timesteps 20000000' launchGPU.sh
-#     # sbatch --array=1-4 --export=ALL,ALG='cleanrl/dqn_atari.py',ENV_ID=$env,ARGSS='--total_timesteps 20000000 --intrinsic_rewards RND --intrinsic_reward_scale=0.2' launchGPU.sh
-#     # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID=$env,ARGSS='--total_timesteps 25000000' launchGPU.sh
-#     # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID=$env,ARGSS='--total_timesteps 25000000 --intrinsic_rewards RND --intrinsic_reward_scale=0.2' launchGPU.sh
-#     sbatch --array=1-4 --export=ALL,ALG='cleanrl/dqn_atari.py',ENV_ID=$env,ARGSS='--network_type ResNet --total_timesteps 50000000' launchGPU.sh
-#     sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID=$env,ARGSS='--network_type ResNet --total_timesteps 50000000' launchGPU.sh
+#     # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--total_timesteps 10000000' launch.sh
+#     sbatch --array=1-5 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=16 --num_envs 4 --total_timesteps 5000000' launch.sh
+#     sbatch --array=1-5 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=32 --num_envs 4 --total_timesteps 5000000' launch.sh
+#     sbatch --array=1-5 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=64 --num_envs 4 --total_timesteps 5000000' launch.sh
+#     sbatch --array=1-5 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=128 --num_envs 4 --total_timesteps 5000000' launch.sh
+#     sbatch --array=1-5 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=256 --num_envs 4 --total_timesteps 5000000' launch.sh
+#     # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_continuous_action.py',ENV_ID=$env,ARGSS='--num_layers=16 --use_layer_norm --num_envs 4 --total_timesteps 5000000' launch.sh
 # done
+
+## Atari RL envs
+strings=(
+    "MontezumaRevengeNoFrameskip-v4"
+    # "AsterixNoFrameskip-v4"
+    # "SpaceInvadersNoFrameskip-v4"
+    # "PitfallNoFrameskip-v4"
+    # "BattleZoneNoFrameskip-v4"
+    # "NameThisGameNoFrameskip-v4"
+    # "PhoenixNoFrameskip-v4"
+)
+for env in "${strings[@]}"; do
+    # echo "$env"
+    # sbatch --array=1-4 --export=ALL,ALG='cleanrl/dqn_atari.py',ENV_ID=$env,ARGSS='--total_timesteps 20000000' launchGPU.sh
+    sbatch --array=1-4 --export=ALL,ALG='cleanrl/dqn_atari.py',ENV_ID=$env,ARGSS='--total_timesteps 25000000 --intrinsic_rewards RND --intrinsic_reward_scale=0.2' launchGPU.sh
+    # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID=$env,ARGSS='--total_timesteps 25000000' launchGPU.sh
+    sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID=$env,ARGSS='--total_timesteps 25000000 --intrinsic_rewards RND --intrinsic_reward_scale=0.2' launchGPU.sh
+    # sbatch --array=1-4 --export=ALL,ALG='cleanrl/dqn_atari.py',ENV_ID=$env,ARGSS='--network_type ResNet --total_timesteps 50000000' launchGPU.sh
+    # sbatch --array=1-4 --export=ALL,ALG='cleanrl/ppo_atari.py',ENV_ID=$env,ARGSS='--network_type ResNet --total_timesteps 50000000' launchGPU.sh
+done
