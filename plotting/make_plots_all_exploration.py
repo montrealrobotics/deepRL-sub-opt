70e89b230f1c50331018df95f1a345773d47dbb8
Neo-X <glen.berseth@mila.quebec>
2025-05-22 14:48:08 -0400

    Working on scaling plots

--- plotting/make_plots_all_exploration.py
+++ plotting/make_plots_all_exploration.py
@@ -11,103 +11,103 @@ if __name__ == '__main__':
 
     res = 50
     lw_ = 3
-    max_ = 25000000
-    fig, (ax3) = plt.subplots(1, 1, figsize=(8,5))
-    #*******************************************************************************
-    #####################
-    ##### Global Optimality #####
-    #####################
-    #*******************************************************************************
+    max_ = 15000
+    # fig, (ax3) = plt.subplots(1, 1, figsize=(8,5))
+    # #*******************************************************************************
+    # #####################
+    # ##### Global Optimality #####
+    # #####################
+    # #*******************************************************************************
     
-    datadir = './data/PPO_MR-all2.csv'
-    df = pd.read_csv(datadir)
-    title = 'Global Optimality Gap on Montezumas Revenge'
-    ax3.set_title(title)
+    # datadir = './data/PPO_MR-all2.csv'
+    # df = pd.read_csv(datadir)
+    # title = 'Global Optimality Gap on Montezumas Revenge'
+    # ax3.set_title(title)
 
-    jobs = [
-        "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833901__2__1747602512",
-        "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833902__3__1747602512",
-        "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833529__4__1747602512",
-        "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833900__1__1747602512",
-    ]
+    # jobs = [
+    #     "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833901__2__1747602512",
+    #     "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833902__3__1747602512",
+    #     "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833529__4__1747602512",
+    #     "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833900__1__1747602512",
+    # ]
 
-    #####################
-    ##### w/ \pi ######
-    #####################
+    # #####################
+    # ##### w/ \pi ######
+    # #####################
     
-    plot_data = get_data_frame(df, key=" - charts/global_optimality_gap", res=res, jobs=jobs, max=max_)
+    # plot_data = get_data_frame(df, key=" - charts/global_optimality_gap", res=res, jobs=jobs, max=max_)
 
-    label='Best $5\%$ - $\pi$ w RND'
-    plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
-    sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
-    ax3.lines[-1].set_linestyle(linestyle[label])
+    # label='$V^{ \hat{\pi}^{*}_{ D_{\infty} } }(s_0) - V^{ \hat{\pi}^{\\theta} }(s_0)$ w RND'
+    # plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
+    # sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
+    # ax3.lines[-1].set_linestyle(linestyle[label])
 
 
-    #####################
-    ##### w/ \pi deterministic ######
-    #####################
+    # #####################
+    # ##### w/ \pi deterministic ######
+    # #####################
     
-    plot_data = get_data_frame(df, key=" - charts/local_optimality_gap", res=res, jobs=jobs, max=max_)
+    # plot_data = get_data_frame(df, key=" - charts/local_optimality_gap", res=res, jobs=jobs, max=max_)
 
-    label='Best $5\%$ from last 1000 episodes - $\pi$ w RND'
-    plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
-    sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
-    ax3.lines[-1].set_linestyle(linestyle[label])
+    # label='$V^{ \hat{\pi}^{*}_{D} }(s_0) - V^{ \hat{\pi}^{\\theta} }(s_0)$ w RND'
+    # plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
+    # sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
+    # ax3.lines[-1].set_linestyle(linestyle[label])
 
-    # datadir = './data/PPO_MR_all2_without_RND.csv'
-    datadir = './data/PPO_MR_All.csv'
-    df = pd.read_csv(datadir)
-    ax3.set_title(title)
+    # # datadir = './data/PPO_MR_all2_without_RND.csv'
+    # datadir = './data/PPO_MR_All.csv'
+    # df = pd.read_csv(datadir)
+    # ax3.set_title(title)
 
-    jobs = [
-        # "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833417__2__1747595815",
-        # "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833418__3__1747595815",
-        # "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833416__1__1747595815",
-        # "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833409__4__1747595815",
-        "MontezumaRevengeNoFrameskip-v4__ppo_atari__6851535__1__1747793950",
-        "MontezumaRevengeNoFrameskip-v4__ppo_atari__6851536__2__1747793950",
-        "MontezumaRevengeNoFrameskip-v4__ppo_atari__6851530__4__1747793950",
-        "MontezumaRevengeNoFrameskip-v4__ppo_atari__6851537__3__1747793950",
+    # jobs = [
+    #     # "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833417__2__1747595815",
+    #     # "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833418__3__1747595815",
+    #     # "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833416__1__1747595815",
+    #     # "MontezumaRevengeNoFrameskip-v4__ppo_atari__6833409__4__1747595815",
+    #     "MontezumaRevengeNoFrameskip-v4__ppo_atari__6851535__1__1747793950",
+    #     "MontezumaRevengeNoFrameskip-v4__ppo_atari__6851536__2__1747793950",
+    #     "MontezumaRevengeNoFrameskip-v4__ppo_atari__6851530__4__1747793950",
+    #     "MontezumaRevengeNoFrameskip-v4__ppo_atari__6851537__3__1747793950",
 
-    ]
+    # ]
 
-    #####################
-    ##### w/ \pi ######
-    #####################
+    # #####################
+    # ##### w/ \pi ######
+    # #####################
     
-    plot_data = get_data_frame(df, key=" - charts/global_optimality_gap", res=res, jobs=jobs, max=max_)
+    # plot_data = get_data_frame(df, key=" - charts/global_optimality_gap", res=res, jobs=jobs)
 
-    label='Best $5\%$ - $\pi$'
-    plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
-    sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
-    ax3.lines[-1].set_linestyle(linestyle[label])
+    # label='$V^{ \hat{\pi}^{*}_{ D_{\infty} } }(s_0) - V^{ \hat{\pi}^{\\theta} }(s_0)$'
+    # plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
+    # sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
+    # ax3.lines[-1].set_linestyle(linestyle[label])
 
 
-    #####################
-    ##### w/ \pi deterministic ######
-    #####################
+    # #####################
+    # ##### w/ \pi deterministic ######
+    # #####################
     
-    plot_data = get_data_frame(df, key=" - charts/local_optimality_gap", res=res, jobs=jobs, max=max_)
+    # plot_data = get_data_frame(df, key=" - charts/local_optimality_gap", res=res, jobs=jobs)
 
-    label='Best $5\%$ from last 1000 episodes - $\pi$'
-    plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
-    sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
-    ax3.lines[-1].set_linestyle(linestyle[label])
+    # label='$V^{ \hat{\pi}^{*}_{D} }(s_0) - V^{ \hat{\pi}^{\\theta} }(s_0)$'
+    # plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
+    # sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
+    # ax3.lines[-1].set_linestyle(linestyle[label])
     
-    ax3.ticklabel_format(axis= 'x', style='sci', scilimits=(0,3))
-    ax3.set(ylabel='Return')
-    ax3.set(xlabel='Steps')
-    ax3.legend()
-    fig.tight_layout(pad=0.5)
-    #plt.subplots_adjust(bottom=.25, wspace=.25)
-    plt.show()
-    fig.savefig("data/"+title+".svg")
-    fig.savefig("data/"+title+".png")
-    fig.savefig("data/"+title+".pdf")
+    # ax3.ticklabel_format(axis= 'x', style='sci', scilimits=(0,3))
+    # ax3.set(ylabel='Return')
+    # ax3.set(xlabel='Steps')
+    # ax3.legend()
+    # fig.tight_layout(pad=0.5)
+    # #plt.subplots_adjust(bottom=.25, wspace=.25)
+    # plt.show()
+    # fig.savefig("data/"+title+".svg")
+    # fig.savefig("data/"+title+".png")
+    # fig.savefig("data/"+title+".pdf")
 
 
 
-    max_ = 25000
+    max_ = 19000
     fig, (ax3) = plt.subplots(1, 1, figsize=(8,5))
     #*******************************************************************************
     #####################
@@ -117,7 +117,7 @@ if __name__ == '__main__':
     
     datadir = './data/PPO_SpaceInvaders_with_RND.csv'
     df = pd.read_csv(datadir)
-    title = 'Global Optimality Gap on Space Invaders'
+    title = 'Global Optimality Gap on SpaceInvaders'
     ax3.set_title(title)
 
     jobs = [
@@ -134,7 +134,7 @@ if __name__ == '__main__':
     
     plot_data = get_data_frame(df, key=" - charts/global_optimality_gap", res=res, jobs=jobs, max=max_)
 
-    label='Best $5\%$ - $\pi$ w RND'
+    label='$V^{ \hat{\pi}^{*}_{ D_{\infty} } }(s_0) - V^{ \hat{\pi}^{\\theta} }(s_0)$ w RND'
     plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
     sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
     ax3.lines[-1].set_linestyle(linestyle[label])
@@ -146,7 +146,7 @@ if __name__ == '__main__':
     
     plot_data = get_data_frame(df, key=" - charts/local_optimality_gap", res=res, jobs=jobs, max=max_)
 
-    label='Best $5\%$ from last 1000 episodes - $\pi$ w RND'
+    label='$V^{ \hat{\pi}^{*}_{D} }(s_0) - V^{ \hat{\pi}^{\\theta} }(s_0)$ w RND'
     plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
     sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
     ax3.lines[-1].set_linestyle(linestyle[label])
@@ -167,9 +167,9 @@ if __name__ == '__main__':
     ##### w/ \pi ######
     #####################
     
-    plot_data = get_data_frame(df, key=" - charts/global_optimality_gap", res=res, jobs=jobs, max=max_)
+    plot_data = get_data_frame(df, key=" - charts/global_optimality_gap", res=res, jobs=jobs)
 
-    label='Best $5\%$ - $\pi$'
+    label='$V^{ \hat{\pi}^{*}_{ D_{\infty} } }(s_0) - V^{ \hat{\pi}^{\\theta} }(s_0)$'
     plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
     sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
     ax3.lines[-1].set_linestyle(linestyle[label])
@@ -179,9 +179,9 @@ if __name__ == '__main__':
     ##### w/ \pi deterministic ######
     #####################
     
-    plot_data = get_data_frame(df, key=" - charts/local_optimality_gap", res=res, jobs=jobs, max=max_)
+    plot_data = get_data_frame(df, key=" - charts/local_optimality_gap", res=res, jobs=jobs)
 
-    label='Best $5\%$ from last 1000 episodes - $\pi$'
+    label='$V^{ \hat{\pi}^{*}_{D} }(s_0) - V^{ \hat{\pi}^{\\theta} }(s_0)$'
     plot_data = plot_data.rename(columns={0: 'Steps', 1: label})
     sns.lineplot(data=plot_data, x='Steps', y=label, ax=ax3, label=label, c=colors[label], linewidth=lw_)
     ax3.lines[-1].set_linestyle(linestyle[label])
